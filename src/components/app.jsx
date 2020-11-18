37cb94d10d629b43118ac956050b2439862fefa8
matt evans <mattevo10@gmail.com>
2020-11-18 16:05:51 +0000

    making progress

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
 import SearchBar from "./search_bar";
 import Gif from "./gif";
+import GifList from "./gif_list";
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "gvw7EmwFDEGIV7Ko1T",
+
+    };
+  }
+
   render() {
     return (
       <div>
-        <div className="left-scence">
+        <div className="left-scene">
           <SearchBar />
           <div className="selected-gif">
-            <Gif />
+            <Gif id={this.state.selectedGifId} />
           </div>
         </div>
-        <div className="right-scence"></div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} />
+        </div>
       </div>
     );
   }
--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 // eslint-disable-next-line react/prefer-stateless-function
 class Gif extends Component {
   render() {
-    const scr = "https://media2.giphy.com/media/gvw7EmwFDEGIV7Ko1T/giphy.gif?cid=ecf05e4757ce45e68c3a31ada3ab2f60259e884ab8e46a4d&rid=giphy.gif";
+    const scr = `https://media2.giphy.com/media/${this.props.id}/giphy.gif?cid=ecf05e47e51801218067dcc606d0eaa0ba2a79d22b7b01f8&rid=giphy.gif`;
     return (
       <img src={scr} alt="" className="gif" />
     );
